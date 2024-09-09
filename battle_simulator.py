972b129147afb3898e1e76c98b5628169c3c453a
Baiqing Lyu <baiqinglyu@bytedance.com>
2024-09-09 14:03:59 -0700

    Use logger

--- battle_simulator.py
+++ battle_simulator.py
@@ -1,17 +1,21 @@
 from poke_env.environment import Battle
 import logging
+from typing import Dict, List, Optional, Union
 
 class BattleSimulator(Battle):
     def __init__(self, battle_tag: str, log_file_path: str):
         # give it a logger
-        logger = logging.getLogger(__name__)
-        self.turn = 0
-        self.p1 = None 
-        self.p2 = None
-        self.winner = ""
-        self.turn_logs = {}
-        self.log_file_path = log_file_path
-        self.parse_message_commands = [
+        self.logger: logging.Logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(logging.StreamHandler())
+
+        self.turn: int = 0
+        self.p1: Optional[str] = None 
+        self.p2: Optional[str] = None
+        self.winner: str = ""
+        self.turn_logs: Dict[int, List[List[str]]] = {}
+        self.log_file_path: str = log_file_path
+        self.parse_message_commands: List[str] = [
             "drag", "switch", "-damage", "move", "cant", "turn", "-heal", "-boost",
             "-weather", "faint", "-unboost", "-ability", "-start", "-activate",
             "-status", "rule", "-clearallboost", "-clearboost", "-clearnegativeboost",
@@ -25,15 +29,16 @@ class BattleSimulator(Battle):
             "-swapsideconditions", "title", "-terastallize"
         ]
         self._parse_log_find_winner()
-        super().__init__(battle_tag=battle_tag, username=self.winner, gen=9, logger=logger)
-    def _parse_log_find_winner(self):
-        current_turn = 0
+        super().__init__(battle_tag=battle_tag, username=self.winner, gen=9, logger=self.logger)
+
+    def _parse_log_find_winner(self) -> None:
+        current_turn: int = 0
 
         with open(self.log_file_path, 'r') as log_file:
             for line in log_file:
                 line = line.strip()
                 if line.startswith('|'):
-                    split_message = line.split('|')[1:]
+                    split_message: List[str] = line.split('|')[1:]
                     if split_message[0] == 'turn':
                         current_turn = int(split_message[1])
                     if current_turn not in self.turn_logs:
@@ -43,8 +48,8 @@ class BattleSimulator(Battle):
                     self.turn_logs[current_turn].append(split_message)
             log_file.close()
     
-    def _register_player_pokemons(self):
-        print("Registering player pokemons")
+    def _register_player_pokemons(self) -> None:
+        self.logger.info("Registering player pokemons")
         for _, messages in self.turn_logs.items():
             for split_message in messages:
                 if split_message[0] == "switch" and split_message[1].startswith(self._player_role):
@@ -53,17 +58,13 @@ class BattleSimulator(Battle):
                     pokemon, move, presumed_target = split_message[1:4]
                     self.get_pokemon(pokemon)._add_move(move)
 
-    def simulate_new_turn(self):
+    def simulate_new_turn(self) -> bool:
         if self.turn >= len(self.turn_logs):
             self._finish_battle()
             return False
 
-        print(f"Turn {self.turn}")
-        messages = self.turn_logs[self.turn]
-
-        # print out the player pokemons
-        for pokemon in self.team.values():
-            print(pokemon.moves)
+        self.logger.info(f"Processing turn {self.turn}")
+        messages: List[List[str]] = self.turn_logs[self.turn]
 
         for split_message in messages:
             if split_message[0] in self.parse_message_commands:
@@ -93,6 +94,6 @@ class BattleSimulator(Battle):
 
 # Usage
 if __name__ == "__main__":
-    battleSimulator = BattleSimulator("log_battle_1", "replay.log")
+    battleSimulator: BattleSimulator = BattleSimulator("log_battle_1", "replay.log")
     while battleSimulator.simulate_new_turn():
         pass
